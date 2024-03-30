355e68adfea6f14b9edb81c607c0341e1021c697
chaejm55 <chaejm55@gmail.com>
2024-03-30 13:24:45 +0900

    Initial commit

---
+++ src/main/java/com/gdsc_knu/official_homepage/OfficialHomepageApplication.java
@@ -0,0 +1,13 @@
+package com.gdsc_knu.official_homepage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfficialHomepageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OfficialHomepageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gdsc_knu/official_homepage/OfficialHomepageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gdsc_knu.official_homepage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OfficialHomepageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

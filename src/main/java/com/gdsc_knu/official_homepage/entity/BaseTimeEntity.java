5d3f512429d85e258b5292175394d5d8372da01b
chaejm55 <chaejm55@gmail.com>
2024-04-01 04:23:11 +0900

    Feat: JPA Auditing 추가

---
+++ src/main/java/com/gdsc_knu/official_homepage/entity/BaseTimeEntity.java
@@ -0,0 +1,22 @@
+package com.gdsc_knu.official_homepage.entity;
+
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseTimeEntity {
+
+    @CreatedDate
+    private LocalDateTime createAt;
+
+    @LastModifiedDate
+    private LocalDateTime modifiedAt;
+}

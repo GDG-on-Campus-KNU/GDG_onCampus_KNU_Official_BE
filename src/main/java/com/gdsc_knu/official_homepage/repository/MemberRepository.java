89f89ebb46b7eaabb6303b27433a49b6baa6e346
chaejm55 <chaejm55@gmail.com>
2024-04-01 20:01:11 +0900

    Feat: 로그인을 위한 레포지토리 메서드 추가

--- src/main/java/com/gdsc_knu/official_homepage/repository/MemberRepository.java
+++ src/main/java/com/gdsc_knu/official_homepage/repository/MemberRepository.java
@@ -4,4 +4,9 @@ import com.gdsc_knu.official_homepage.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
+    Member findByEmailAndStudentNumber(String email, String studentNumber);
+
+    Member findByEmail(String email);
+
+    Member findBySessionId(String sessionId);
 }

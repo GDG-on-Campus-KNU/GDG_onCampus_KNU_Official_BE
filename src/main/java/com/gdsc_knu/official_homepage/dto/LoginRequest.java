469861f341ff5b67938459f8af129c0c55c744d6
chaejm55 <chaejm55@gmail.com>
2024-04-01 20:02:30 +0900

    Feat: 임시 세션 로그인/로그아웃 API 기능 구현

---
+++ src/main/java/com/gdsc_knu/official_homepage/dto/LoginRequest.java
@@ -0,0 +1,18 @@
+package com.gdsc_knu.official_homepage.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class LoginRequest {
+    @NotBlank
+    private String email;
+    @NotBlank
+    private String studentNumber;
+}

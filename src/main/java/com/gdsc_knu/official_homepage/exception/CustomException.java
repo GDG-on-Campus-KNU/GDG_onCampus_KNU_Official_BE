068c524afac3bd5e69b775263c225815468dbe37
kwonssshyeon <kwonsh4396@gmail.com>
2024-04-02 13:19:21 +0900

    Feat: exception handler 추가

---
+++ src/main/java/com/gdsc_knu/official_homepage/exception/CustomException.java
@@ -0,0 +1,16 @@
+package com.gdsc_knu.official_homepage.exception;
+
+import lombok.Getter;
+
+@Getter
+public class CustomException extends RuntimeException {
+    private int result;
+    private ErrorCode errorCode;
+    private String message;
+
+    public CustomException(ErrorCode errorCode) {
+        this.result = errorCode.getStatus();
+        this.errorCode = errorCode;
+        this.message = errorCode.getMessage();
+    }
+}
---
+++ src/main/java/com/gdsc_knu/official_homepage/exception/ExceptionDto.java
@@ -0,0 +1,24 @@
+package com.gdsc_knu.official_homepage.exception;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public class ExceptionDto {
+    private int code;
+    private String message;
+    private HttpStatus data;
+
+    public ExceptionDto(ErrorCode error) {
+        this.code = error.getStatus();
+        this.message = error.getMessage();
+        this.data = error.getError();
+    }
+
+    public ExceptionDto(ErrorCode error, String message) {
+        this.code = error.getStatus();
+        this.message = message;
+        this.data = error.getError();
+    }
+
+}

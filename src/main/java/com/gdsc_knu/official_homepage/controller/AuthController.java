5e87eee99978027da3904e3e8674cad78a1e9362
chaejm55 <chaejm55@gmail.com>
2024-04-08 19:58:42 +0900

    Fix: 세션 기반 컨트롤러 주석화

--- src/main/java/com/gdsc_knu/official_homepage/controller/AuthController.java
+++ src/main/java/com/gdsc_knu/official_homepage/controller/AuthController.java
@@ -2,10 +2,7 @@ package com.gdsc_knu.official_homepage.controller;
 
 import com.gdsc_knu.official_homepage.dto.LoginRequest;
 import com.gdsc_knu.official_homepage.dto.SignupRequest;
-import com.gdsc_knu.official_homepage.service.MemberService;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.servlet.http.Cookie;
-import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -19,23 +16,23 @@ import org.springframework.http.HttpHeaders;
 @RequestMapping("/api/auth")
 @RequiredArgsConstructor
 public class AuthController {
-    private final MemberService memberService;
-
-    @PostMapping("/signup")
-    public ResponseEntity<Long> signup(@RequestBody @Valid SignupRequest signupRequest) {
-        Long memberId = memberService.signup(signupRequest);
-        return ResponseEntity.status(HttpStatus.CREATED).body(memberId);
-    }
-
-    @PostMapping("/login")
-    public ResponseEntity<Void> login(@RequestBody @Valid LoginRequest loginRequest) {
-        HttpHeaders headers = memberService.login(loginRequest);
-        return ResponseEntity.ok().headers(headers).build();
-    }
-
-    @PostMapping("/logout")
-    public ResponseEntity<Void> logout(@CookieValue(name = "JSESSIONID") String sessionId) {
-        HttpHeaders headers = memberService.logout(sessionId);
-        return ResponseEntity.ok().headers(headers).build();
-    }
+//    private final MemberService memberService;
+//
+//    @PostMapping("/signup")
+//    public ResponseEntity<Long> signup(@RequestBody @Valid SignupRequest signupRequest) {
+//        Long memberId = memberService.signup(signupRequest);
+//        return ResponseEntity.status(HttpStatus.CREATED).body(memberId);
+//    }
+//
+//    @PostMapping("/login")
+//    public ResponseEntity<Void> login(@RequestBody @Valid LoginRequest loginRequest) {
+//        HttpHeaders headers = memberService.login(loginRequest);
+//        return ResponseEntity.ok().headers(headers).build();
+//    }
+//
+//    @PostMapping("/logout")
+//    public ResponseEntity<Void> logout(@CookieValue(name = "JSESSIONID") String sessionId) {
+//        HttpHeaders headers = memberService.logout(sessionId);
+//        return ResponseEntity.ok().headers(headers).build();
+//    }
 }

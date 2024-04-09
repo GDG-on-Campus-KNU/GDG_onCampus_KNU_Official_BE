bc08c4bbfbeda04076443688162a5ae45c481834
kwonssshyeon <kwonsh4396@gmail.com>
2024-04-09 17:38:19 +0900

    Feat: CORS 허용 설정

---
+++ src/main/java/com/gdsc_knu/official_homepage/config/WebConfig.java
@@ -0,0 +1,17 @@
+package com.gdsc_knu.official_homepage.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("*")
+                .allowedHeaders("*");
+
+    }
+}

05d2dc4e48afce82c242746b34f9f545216c41b6
chaejm55 <chaejm55@gmail.com>
2024-04-11 14:01:41 +0900

    Feat: Jwt 기능 테스트용 컨트롤러 추가(추후 삭제 필요)

---
+++ src/main/java/com/gdsc_knu/official_homepage/controller/JwtTestController.java
@@ -0,0 +1,36 @@
+package com.gdsc_knu.official_homepage.controller;
+
+import com.gdsc_knu.official_homepage.authentication.JwtTokenProvider;
+import com.gdsc_knu.official_homepage.authentication.JwtTokenValidator;
+import com.gdsc_knu.official_homepage.dto.jwt.TokenResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RequestMapping("/api/jwt")
+@RestController
+@RequiredArgsConstructor
+public class JwtTestController {
+    private final JwtTokenProvider jwtTokenProvider;
+    private final JwtTokenValidator jwtTokenValidator;
+
+    // chaejm55@gmail.com으로 jwt 토큰을 발급하는 테스트 컨트롤러입니다.
+    // 현재 서버에 chaejm55@gmail.com 이메일 멤버가 입력되어있습니다.
+    @GetMapping
+    public ResponseEntity<TokenResponse> getJwtToken() {
+        return ResponseEntity.ok().body(jwtTokenProvider.issueTokens("chaejm55@gmail.com"));
+    }
+
+    // 전달 받은 엑세스 토큰으로 멤버 이메일, 역할을 반환하는 테스트 컨트롤러입니다.
+    @GetMapping("/check")
+    public ResponseEntity<JwtTokenValidator.JwtUserDetails> checkJwtToken(@RequestHeader("Authorization") String token) {
+        String checkedToken = jwtTokenValidator.checkAccessToken(token);
+        return ResponseEntity.ok().body(jwtTokenValidator.getUserDetails(checkedToken));
+    }
+
+    // 리프레시 토큰으로 모든 토큰을 재발급 받는 테스트 컨트롤러입니다.
+    @GetMapping("/reissue")
+    public ResponseEntity<TokenResponse> reissueTokens(@RequestHeader("Authorization") String token) {
+        return ResponseEntity.ok().body(jwtTokenProvider.reissueTokens(token));
+    }
+}

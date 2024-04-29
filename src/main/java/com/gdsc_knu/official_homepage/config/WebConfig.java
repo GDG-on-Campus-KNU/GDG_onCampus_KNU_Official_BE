8f84704888a30a0519aa87a77c6505a2e1b0184b
chaejm55 <chaejm55@gmail.com>
2024-04-30 02:09:38 +0900

    Feat: Resolver 등록

--- src/main/java/com/gdsc_knu/official_homepage/config/WebConfig.java
+++ src/main/java/com/gdsc_knu/official_homepage/config/WebConfig.java
@@ -1,11 +1,20 @@
 package com.gdsc_knu.official_homepage.config;
 
+import com.gdsc_knu.official_homepage.annotation.TokenMemberResolver;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.List;
+
 @Configuration
+@RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
+
+    private final TokenMemberResolver tokenMemberResolver;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
@@ -13,5 +22,10 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedMethods("*")
                 .allowedHeaders("*");
 
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(tokenMemberResolver);
     }
 }

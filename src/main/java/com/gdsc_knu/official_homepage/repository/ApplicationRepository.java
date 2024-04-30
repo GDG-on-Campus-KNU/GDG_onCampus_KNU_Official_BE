30d281240d6ac1d17339d5acf1f0d5ee73b8c40d
chaejm55 <chaejm55@gmail.com>
2024-04-30 21:37:45 +0900

    Feat: 지원서 레포지토리 작성

---
+++ src/main/java/com/gdsc_knu/official_homepage/repository/ApplicationRepository.java
@@ -0,0 +1,12 @@
+package com.gdsc_knu.official_homepage.repository;
+
+import com.gdsc_knu.official_homepage.entity.Application;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ApplicationRepository extends JpaRepository<Application, Long> {
+    Optional<Application> findByStudentNumber(String studentNumber);
+}

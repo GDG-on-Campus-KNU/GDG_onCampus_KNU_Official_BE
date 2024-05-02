6715b794943aa0c160d62c953ea7d82ddfce8d14
kwonssshyeon <kwonsh4396@gmail.com>
2024-05-02 17:39:07 +0900

    Feat: Jwt 필터 등록

--- src/main/java/com/gdsc_knu/official_homepage/config/SecurityConfig.java
+++ src/main/java/com/gdsc_knu/official_homepage/config/SecurityConfig.java
@@ -1,14 +1,20 @@
 package com.gdsc_knu.official_homepage.config;
 
+import com.gdsc_knu.official_homepage.authentication.JwtFilter;
+import com.gdsc_knu.official_homepage.authentication.JwtTokenValidator;
+import com.gdsc_knu.official_homepage.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
 import org.springframework.security.web.util.matcher.MediaTypeRequestMatcher;
 import org.springframework.web.servlet.HandlerMapping;
@@ -19,22 +25,28 @@ import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
 @EnableMethodSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
+    private final JwtTokenValidator jwtTokenValidator;
+    private final MemberRepository memberRepository;
+
     private static final String[] WHITE_LIST = {
             "/**"
     };
     private static final String[] MEMBER_AUTHENTICATION_LIST = {
-
+            "/api/jwt/member"
     };
 
     private static final String[] CORE_AUTHENTICATION_LIST = {
-
+            "/api/jwt/core"
     };
+
     @Bean
     protected SecurityFilterChain filterChain(HttpSecurity httpSecurity, HandlerMappingIntrospector introspector) throws Exception {
         httpSecurity
                 .csrf(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .addFilterBefore(new JwtFilter(jwtTokenValidator,memberRepository), UsernamePasswordAuthenticationFilter.class)
                 .authorizeHttpRequests(authorizeRequest -> authorizeRequest
-                        .requestMatchers(WHITE_LIST).permitAll()
                         .requestMatchers(MEMBER_AUTHENTICATION_LIST).hasRole("MEMBER")
                         .requestMatchers(CORE_AUTHENTICATION_LIST).hasRole("CORE")
                         .anyRequest().permitAll()

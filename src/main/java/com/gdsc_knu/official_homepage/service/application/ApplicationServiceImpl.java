887bbf5b0fb89718116c1954bffdf64a4fc937b7
chaejm55 <chaejm55@gmail.com>
2024-05-04 23:44:12 +0900

    Fix: Service에 Transactional 추가

--- src/main/java/com/gdsc_knu/official_homepage/service/application/ApplicationServiceImpl.java
+++ src/main/java/com/gdsc_knu/official_homepage/service/application/ApplicationServiceImpl.java
@@ -6,12 +6,14 @@ import com.gdsc_knu.official_homepage.entity.Application;
 import com.gdsc_knu.official_homepage.repository.ApplicationRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class ApplicationServiceImpl implements ApplicationService {
     private final ApplicationRepository applicationRepository;
     @Override
+    @Transactional(readOnly = true)
     public ApplicationResponse getApplication(String name, String studentNumber) {
         Application application = applicationRepository.findByNameAndStudentNumber(name, studentNumber)
                 .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 지원서입니다."));
@@ -19,11 +21,13 @@ public class ApplicationServiceImpl implements ApplicationService {
     }
 
     @Override
+    @Transactional
     public Long saveApplication(ApplicationRequest applicationRequest) {
         return applicationRepository.save(applicationRequest.toEntity()).getId();
     }
 
     @Override
+    @Transactional
     public Long updateApplication(ApplicationRequest applicationRequest) {
         Application application = applicationRepository.findByNameAndStudentNumber(applicationRequest.getName(), applicationRequest.getStudentNumber())
                 .orElseThrow(() -> new IllegalArgumentException("해당 지원서가 존재하지 않습니다."));

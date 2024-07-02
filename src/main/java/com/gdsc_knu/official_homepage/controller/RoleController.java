d8d0d67e6b2c3fb9e1f45fcdcbb236d66e12ca90
kwonssshyeon <kwonsh4396@gmail.com>
2024-07-02 10:19:56 +0900

    Feat: 멤버 승인 API 구현

--- src/main/java/com/gdsc_knu/official_homepage/controller/RoleController.java
+++ src/main/java/com/gdsc_knu/official_homepage/controller/RoleController.java
@@ -1,2 +1,27 @@
-package com.gdsc_knu.official_homepage.controller;public class RoleController {
+package com.gdsc_knu.official_homepage.controller;
+
+import com.gdsc_knu.official_homepage.annotation.TokenMember;
+import com.gdsc_knu.official_homepage.authentication.jwt.JwtMemberDetail;
+import com.gdsc_knu.official_homepage.dto.role.UserRoleRequest;
+import com.gdsc_knu.official_homepage.service.PermissionSerivce;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@Tag(name = "Permission", description = "사용자 상태(ROLE) 관련 API")
+@RestController
+@RequestMapping("api/user/permission")
+@RequiredArgsConstructor
+public class RoleController {
+    private final PermissionSerivce permissionSerivce;
+    @PatchMapping("approve")
+    @Operation(summary="MEMBER 승인 API")
+    public ResponseEntity<String> approveMember(@TokenMember JwtMemberDetail jwtMemberDetail,
+                                        @RequestBody UserRoleRequest userRoleRequest){
+        Long count = permissionSerivce.approveMember(userRoleRequest);
+        return ResponseEntity.ok().body(count+"명의 멤버가 승인되었습니다.");
+    }
+
 }
--- src/main/java/com/gdsc_knu/official_homepage/service/PermissionSerivce.java
+++ src/main/java/com/gdsc_knu/official_homepage/service/PermissionSerivce.java
@@ -1,2 +1,35 @@
-package com.gdsc_knu.official_homepage.service;public class PermissionSerivce {
+package com.gdsc_knu.official_homepage.service;
+
+import com.gdsc_knu.official_homepage.dto.role.UserRoleRequest;
+import com.gdsc_knu.official_homepage.entity.Member;
+import com.gdsc_knu.official_homepage.entity.enumeration.Role;
+import com.gdsc_knu.official_homepage.exception.CustomException;
+import com.gdsc_knu.official_homepage.exception.ErrorCode;
+import com.gdsc_knu.official_homepage.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class PermissionSerivce {
+    private final MemberRepository memberRepository;
+
+    /**
+     * GUEST에서 MEMBER로 승인
+     * ROLE이 GUEST가 아닌 경우 무시
+     * 정상적으로 승인된 USER의 수를 반환한다.
+     */
+    @Transactional
+    public Long approveMember(UserRoleRequest request){
+        List<Long> userIds = request.getUserIds();
+        return userIds.stream()
+                .map(userId -> memberRepository.findById(userId)
+                        .orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND)))
+                .filter(member -> member.getRole() == Role.ROLE_GUEST)
+                .peek(member -> member.updateRole(Role.ROLE_MEMBER))
+                .count();
+    }
 }

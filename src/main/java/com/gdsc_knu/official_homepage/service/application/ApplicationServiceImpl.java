8ee543a0b6bff74fec63f4937078de56d99783d8
chaejm55 <chaejm55@gmail.com>
2024-07-13 19:58:03 +0900

    Refactor: 중복 예외 처리 함수로 추출, CustomException으로 예외 통일

--- src/main/java/com/gdsc_knu/official_homepage/service/application/ApplicationServiceImpl.java
+++ src/main/java/com/gdsc_knu/official_homepage/service/application/ApplicationServiceImpl.java
@@ -22,29 +22,23 @@ public class ApplicationServiceImpl implements ApplicationService {
     @Override
     @Transactional(readOnly = true)
     public ApplicationResponse getApplication(String email, String name, String studentNumber) {
-        Member member = memberRepository.findByEmail(email)
-                .orElseThrow(() -> new IllegalArgumentException("올바르지 않은 회원정보입니다."));
+        Member member = validateMember(email);
         if (!member.getStudentNumber().equals(studentNumber)) {
-            throw new IllegalArgumentException("본인의 지원서만 조회 가능합니다.");
+            throw new CustomException(ErrorCode.FORBIDDEN,"본인의 지원서만 접근 가능합니다.");
         }
-        Application application = applicationRepository.findByNameAndStudentNumber(name, studentNumber)
-                .orElseThrow(() -> new IllegalArgumentException("작성하신 지원서가 없습니다."));
-        if (!application.getApplicationStatus().equals(ApplicationStatus.TEMPORAL)) {
-            throw new CustomException(ErrorCode.CONFLICT);
-        }
+        Application application = validateApplicationAccess(name, studentNumber);
         return new ApplicationResponse(application);
     }
 
     @Override
     @Transactional
     public Long saveApplication(String email, ApplicationRequest applicationRequest) {
-        checkValidApplicationStatus(applicationRequest.getApplicationStatus());
+        validateApplicationStatus(applicationRequest.getApplicationStatus());
         // 새로운 로직
-        Member member = memberRepository.findByEmail(email)
-                .orElseThrow(() -> new IllegalArgumentException("올바르지 않은 회원정보입니다."));
+        Member member = validateMember(email);
         applicationRepository.findByNameAndStudentNumber(member.getName(), member.getStudentNumber())
                 .ifPresent(application -> {
-                    throw new IllegalArgumentException("이미 작성한 지원서가 존재합니다.");
+                    throw new CustomException(ErrorCode.CONFLICT, "이미 작성한 지원서가 존재합니다.");
                 });
         return applicationRepository.save(new Application(member, applicationRequest)).getId();
     }
@@ -52,22 +46,31 @@ public class ApplicationServiceImpl implements ApplicationService {
     @Override
     @Transactional
     public Long updateApplication(String email, ApplicationRequest applicationRequest) {
-        checkValidApplicationStatus(applicationRequest.getApplicationStatus());
-        Member member = memberRepository.findByEmail(email)
-                .orElseThrow(() -> new IllegalArgumentException("올바르지 않은 회원정보입니다."));
-        Application application = applicationRepository.findByNameAndStudentNumber(member.getName(), member.getStudentNumber())
-                .orElseThrow(() -> new IllegalArgumentException("해당 지원서가 존재하지 않습니다."));
-        if (!application.getApplicationStatus().equals(ApplicationStatus.TEMPORAL)) {
-            throw new CustomException(ErrorCode.CONFLICT);
-        }
+        validateApplicationStatus(applicationRequest.getApplicationStatus());
+        Member member = validateMember(email);
+        Application application = validateApplicationAccess(member.getName(), member.getStudentNumber());
         application.updateApplication(member, applicationRequest);
         applicationRepository.save(application);
         return application.getId();
     }
 
-    private void checkValidApplicationStatus(ApplicationStatus applicationStatus) {
+    private void validateApplicationStatus(ApplicationStatus applicationStatus) {
         if (applicationStatus.equals(ApplicationStatus.REJECTED) || applicationStatus.equals(ApplicationStatus.APPROVED)) {
-            throw new IllegalArgumentException("잘못된 요청입니다.");
+            throw new CustomException(ErrorCode.INVALID_INPUT, "올바르지 않은 지원서 요청입니다.");
         }
+    }
+
+    private Member validateMember (String email) {
+        return memberRepository.findByEmail(email)
+                .orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND));
+    }
+
+    private Application validateApplicationAccess (String name, String studentNumber) {
+        Application application = applicationRepository.findByNameAndStudentNumber(name, studentNumber)
+                .orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND));
+        if (!application.getApplicationStatus().equals(ApplicationStatus.TEMPORAL)) {
+            throw new CustomException(ErrorCode.CONFLICT);
+        }
+        return application;
     }
 }

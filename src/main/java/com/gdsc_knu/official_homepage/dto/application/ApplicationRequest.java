7ec0df2d65ab27fc476910999df376350911049d
chaejm55 <chaejm55@gmail.com>
2024-07-15 15:38:36 +0900

    Refactor: 의미 없는 코드 최적화

--- src/main/java/com/gdsc_knu/official_homepage/dto/application/ApplicationRequest.java
+++ src/main/java/com/gdsc_knu/official_homepage/dto/application/ApplicationRequest.java
@@ -1,16 +1,8 @@
 package com.gdsc_knu.official_homepage.dto.application;
 
-import com.gdsc_knu.official_homepage.annotation.PhoneNumber;
 import com.gdsc_knu.official_homepage.annotation.ValidEnum;
-import com.gdsc_knu.official_homepage.entity.application.Application;
-import com.gdsc_knu.official_homepage.entity.application.ApplicationAnswer;
 import com.gdsc_knu.official_homepage.entity.enumeration.ApplicationStatus;
 import com.gdsc_knu.official_homepage.entity.enumeration.Track;
-import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -18,7 +10,6 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @NoArgsConstructor
 @AllArgsConstructor
@@ -31,11 +22,9 @@ public class ApplicationRequest {
     @NotNull(message = "잘못된 입력 형식입니다.")
     private String links;
 
-    @Enumerated(EnumType.STRING)
     @ValidEnum(enumClass = ApplicationStatus.class, message = "올바르지 않은 지원상태입니다.")
     private ApplicationStatus applicationStatus;
 
-    @Enumerated(EnumType.STRING)
     @ValidEnum(enumClass = Track.class, message = "올바르지 않은 트랙입니다.")
     private Track track;
 

ef671f6a1e8105b233ea6487e1956c3236464f75
kwonssshyeon <kwonsh4396@gmail.com>
2024-07-18 17:30:19 +0900

    Feat: member request 유효성 검사

--- src/main/java/com/gdsc_knu/official_homepage/dto/member/MemberRequest.java
+++ src/main/java/com/gdsc_knu/official_homepage/dto/member/MemberRequest.java
@@ -1,19 +1,27 @@
 package com.gdsc_knu.official_homepage.dto.member;
 
-import com.gdsc_knu.official_homepage.entity.enumeration.Track;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 public class MemberRequest {
 
     @Getter
     @AllArgsConstructor
     public static class Append {
+        @NotBlank(message = "이름을 입력해주세요.")
         private String name;
+
+        @NotBlank(message = "나이를 입력해주세요.")
         private int age;
+
+        @NotBlank(message = "학번을 입력해주세요.")
         private String studentNumber;
+
+        @NotBlank(message = "전공을 입력해주세요.")
         private String major;
+
+        @NotBlank(message = "전화번호를 입력해주세요.")
         private String phoneNumber;
     }
 

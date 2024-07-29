9e1d99c3b7f72fb4b153aa28ff6b251940b5b668
chaejm55 <chaejm55@gmail.com>
2024-07-30 03:09:22 +0900

    Fix: DTO에 누락된 생성자 추가

--- src/main/java/com/gdsc_knu/official_homepage/dto/admin/memberStatus/MemberRoleRequest.java
+++ src/main/java/com/gdsc_knu/official_homepage/dto/admin/memberStatus/MemberRoleRequest.java
@@ -1,13 +1,17 @@
 package com.gdsc_knu.official_homepage.dto.admin.memberStatus;
 
 import com.gdsc_knu.official_homepage.entity.enumeration.Role;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Getter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class MemberRoleRequest {
     private List<Long> userIds;
     private Role role;
--- src/main/java/com/gdsc_knu/official_homepage/dto/admin/memberStatus/MemberTrackRequest.java
+++ src/main/java/com/gdsc_knu/official_homepage/dto/admin/memberStatus/MemberTrackRequest.java
@@ -1,14 +1,17 @@
 package com.gdsc_knu.official_homepage.dto.admin.memberStatus;
 
-import com.gdsc_knu.official_homepage.entity.enumeration.Role;
 import com.gdsc_knu.official_homepage.entity.enumeration.Track;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Getter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class MemberTrackRequest {
     private List<Long> userIds;
     private Track track;

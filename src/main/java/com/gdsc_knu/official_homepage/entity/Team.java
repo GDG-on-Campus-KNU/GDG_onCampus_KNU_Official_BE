bf1193b933236da14bec27aadf646f987a8867ae
chaejm55 <chaejm55@gmail.com>
2024-08-01 15:28:35 +0900

    Feat: 하위 팀 지정을 위한 연관 관계, 편의 메서드 추가

--- src/main/java/com/gdsc_knu/official_homepage/entity/Team.java
+++ src/main/java/com/gdsc_knu/official_homepage/entity/Team.java
@@ -8,6 +8,9 @@ import java.util.List;
 
 @Entity
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
 public class Team {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -17,4 +20,20 @@ public class Team {
 
     @OneToMany(mappedBy = "team")
     private List<MemberTeam> memberTeams = new ArrayList<>();
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "parent_id")
+    private Team parent;
+
+    @OneToMany(mappedBy = "parent", cascade = CascadeType.ALL)
+    private List<Team> subTeams = new ArrayList<>();
+
+    public void addSubTeam(Team subteam) {
+        subTeams.add(subteam);
+        subteam.setParent(this);
+    }
+
+    public void setParent(Team parent) {
+        this.parent = parent;
+    }
 }

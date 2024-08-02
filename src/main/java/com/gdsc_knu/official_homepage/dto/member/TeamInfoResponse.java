fad6ba93a1e78dc26e378bfdb25377f4df2e8b6e
chaejm55 <chaejm55@gmail.com>
2024-08-02 10:40:16 +0900

    Feat: TeamInfoResponseDTO에 편의 메서드 추가

--- src/main/java/com/gdsc_knu/official_homepage/dto/member/TeamInfoResponse.java
+++ src/main/java/com/gdsc_knu/official_homepage/dto/member/TeamInfoResponse.java
@@ -1,11 +1,23 @@
 package com.gdsc_knu.official_homepage.dto.member;
 
+import com.gdsc_knu.official_homepage.entity.Team;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class TeamInfoResponse {
+    private Long id;
     private String teamName;
     private String teamPageUrl;
+
+    public TeamInfoResponse(Team team) {
+        this.id = team.getId();
+        this.teamName = team.getTeamName();
+        this.teamPageUrl = team.getTeamPageUrl();
+    }
 }

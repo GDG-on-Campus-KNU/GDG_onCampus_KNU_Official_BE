c2852d4d771ff1b472f0ce2dc146fcc554dad908
chaejm55 <chaejm55@gmail.com>
2024-08-02 10:41:44 +0900

    Feat: subTeams 필드 추가 및 DTO 이름 변경

--- src/main/java/com/gdsc_knu/official_homepage/dto/admin/team/AdminTeamResponse.java
+++ src/main/java/com/gdsc_knu/official_homepage/dto/admin/team/AdminTeamResponse.java
@@ -1,16 +1,22 @@
 package com.gdsc_knu.official_homepage.dto.admin.team;
 
+import com.gdsc_knu.official_homepage.dto.member.TeamInfoResponse;
+import com.gdsc_knu.official_homepage.entity.Team;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class AdminTeamInfoResponse {
+public class AdminTeamResponse {
     private Long id;
     private String teamName;
     private String teamPageUrl;
+    private List<TeamInfoResponse> subTeams = new ArrayList<>();
 }

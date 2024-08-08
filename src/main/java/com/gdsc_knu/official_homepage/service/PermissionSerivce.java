3c416166e6a0c492406054b0fd0a17d5f66d282e
kwonssshyeon <kwonsh4396@gmail.com>
2024-08-08 21:51:09 +0900

    Feat: Core 권한 임시 획득 메서드 구현

--- src/main/java/com/gdsc_knu/official_homepage/service/PermissionSerivce.java
+++ src/main/java/com/gdsc_knu/official_homepage/service/PermissionSerivce.java
@@ -17,19 +17,11 @@ import java.util.List;
 public class PermissionSerivce {
     private final MemberRepository memberRepository;
 
-    /**
-     * GUEST에서 MEMBER로 승인
-     * ROLE이 GUEST가 아닌 경우 무시
-     * 정상적으로 승인된 USER의 수를 반환한다.
-     */
+    //❗삭제 예정❗
     @Transactional
-    public Long approveMember(UserRoleRequest request){
-        List<Long> userIds = request.getUserIds();
-        return userIds.stream()
-                .map(userId -> memberRepository.findById(userId)
-                        .orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND)))
-                .filter(member -> member.getRole() == Role.ROLE_GUEST)
-                .peek(member -> member.updateRole(Role.ROLE_MEMBER))
-                .count();
+    public void toCore(Long id){
+        Member member = memberRepository.findById(id)
+                .orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND));
+        member.updateRole(Role.ROLE_CORE);
     }
 }

58e8b2a461578bc86f8583d43edeaa9a85602541
chaejm55 <chaejm55@gmail.com>
2024-08-13 18:28:59 +0900

    Comment: 멤버 상태조정 컨트롤러에 description 추가

--- src/main/java/com/gdsc_knu/official_homepage/controller/admin/AdminMemberStatusController.java
+++ src/main/java/com/gdsc_knu/official_homepage/controller/admin/AdminMemberStatusController.java
@@ -22,15 +22,15 @@ public class AdminMemberStatusController {
     private final AdminMemberStatusService adminMemberStatusService;
 
     @GetMapping()
-    @Operation(summary = "모든 멤버 정보 조회 API")
+    @Operation(summary = "모든 멤버 정보 조회 API", description = "모든 멤버의 정보를 조회 합니다.")
     public ResponseEntity<PagingResponse<MemberInfoResponse>> getAllMemberInfos(
             @RequestParam(value = "page", defaultValue = "0") int page,
             @RequestParam(value = "size", defaultValue = "10") int size) {
         return ResponseEntity.ok(adminMemberStatusService.getAllMemberInfos(page, size));
     }
 
-    @GetMapping("search")
-    @Operation(summary = "이름검색으로 멤버 정보 조회 API")
+    @GetMapping("/search")
+    @Operation(summary = "이름 검색으로 멤버 정보 조회 API", description = "요청한 이름을 포함 하는 모든 멤버의 정보를 조회합니다.")
     public ResponseEntity<PagingResponse<MemberInfoResponse>> getMemberByName(
             @RequestParam(value = "name") String name,
             @RequestParam(value = "page", defaultValue = "0") int page,
@@ -39,20 +39,20 @@ public class AdminMemberStatusController {
     }
 
     @DeleteMapping()
-    @Operation(summary = "회원 삭제 API")
+    @Operation(summary = "회원 삭제 API", description = "요청한 멤버들을 일괄 삭제(회원 탈퇴)합니다.")
     public ResponseEntity<Void> deleteMember(@RequestBody MemberDeleteRequest memberDeleteRequest) {
         adminMemberStatusService.deleteMember(memberDeleteRequest);
         return ResponseEntity.ok().build();
     }
 
     @PatchMapping("/role")
-    @Operation(summary = "회원 권한 수정 API")
+    @Operation(summary = "회원 권한 수정 API", description = "요청한 멤버들의 권한을 일괄 변경합니다.")
     public ResponseEntity<Long> updateMemberRole(@RequestBody MemberRoleRequest memberRoleRequest) {
         return ResponseEntity.ok(adminMemberStatusService.updateMemberRole(memberRoleRequest));
     }
 
     @PatchMapping("/track")
-    @Operation(summary = "회원 직렬 수정 API")
+    @Operation(summary = "회원 직렬 수정 API", description = "요청한 멤버들의 직렬을 일괄 변경합니다.")
     public ResponseEntity<Long> updateMemberTrack(@RequestBody MemberTrackRequest memberTrackRequest) {
         return ResponseEntity.ok(adminMemberStatusService.updateMemberTrack(memberTrackRequest));
     }

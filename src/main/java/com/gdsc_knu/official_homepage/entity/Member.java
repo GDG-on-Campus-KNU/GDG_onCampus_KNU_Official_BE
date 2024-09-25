78693cb03bea7f6469f91aa04013909e054460ff
kwonssshyeon <kwonsh4396@gmail.com>
2024-09-25 16:22:43 +0900

    Feat: 멤버 엔티티 - 직렬 UNDEFINED 추가

--- src/main/java/com/gdsc_knu/official_homepage/entity/Member.java
+++ src/main/java/com/gdsc_knu/official_homepage/entity/Member.java
@@ -19,6 +19,7 @@ public class Member extends BaseTimeEntity{
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
     private String name;
 
     private int age;
@@ -27,7 +28,7 @@ public class Member extends BaseTimeEntity{
 
     private String major;
 
-    @Column(unique = true)
+    @Column(nullable = false, unique = true)
     private String email;
 
     @Column(unique = true)
@@ -40,10 +41,12 @@ public class Member extends BaseTimeEntity{
     private String profileUrl;
 
     @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
     private Track track;
 
     @OneToMany(mappedBy = "member")
     private final List<MemberTeam> memberTeams = new ArrayList<>();
+
     public void updateRole(Role role) {
         this.role = role;
     }

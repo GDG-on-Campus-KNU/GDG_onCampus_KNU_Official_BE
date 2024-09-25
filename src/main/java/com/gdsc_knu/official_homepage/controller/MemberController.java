e6ce8800ddedfe3be063d7bd42058115d6d03cdd
kwonssshyeon <kwonsh4396@gmail.com>
2024-09-25 18:25:23 +0900

    Feat: 사용자 권한 변경 편의 API 구현

--- src/main/java/com/gdsc_knu/official_homepage/controller/MemberController.java
+++ src/main/java/com/gdsc_knu/official_homepage/controller/MemberController.java
@@ -5,6 +5,9 @@ import com.gdsc_knu.official_homepage.authentication.jwt.JwtMemberDetail;
 import com.gdsc_knu.official_homepage.dto.member.MemberRequest;
 import com.gdsc_knu.official_homepage.dto.member.MemberResponse;
 import com.gdsc_knu.official_homepage.dto.member.TeamInfoResponse;
+import com.gdsc_knu.official_homepage.entity.Member;
+import com.gdsc_knu.official_homepage.entity.enumeration.Role;
+import com.gdsc_knu.official_homepage.entity.enumeration.Track;
 import com.gdsc_knu.official_homepage.service.MemberInfoService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -14,13 +17,13 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@Tag(name = "Member", description = "사용자 정보 관련 API")
+@Tag(name = "User", description = "사용자 정보 관련 API")
 @RestController
 @RequestMapping("api/user")
 @RequiredArgsConstructor
 public class MemberController {
     private final MemberInfoService memberInfoService;
-    @PostMapping("additional-info")
+    @PutMapping()
     @Operation(summary="신규가입 추가정보 입력 API")
     public void additionalInfo(@TokenMember JwtMemberDetail jwtMemberDetail,
                                @RequestBody MemberRequest.Append request){
@@ -32,5 +35,14 @@ public class MemberController {
     public ResponseEntity<MemberResponse> getMemberInfo(@TokenMember JwtMemberDetail jwtMemberDetail){
         return ResponseEntity.ok().body(memberInfoService.getMemberInfo(jwtMemberDetail.getId()));
     }
+
+    @PatchMapping()
+    @Operation(summary="직렬, 권한 변경 편의 API (❗개발 API 아님❗)")
+    public ResponseEntity<Member> getMember(@RequestParam(required = true) Track track,
+                                            @RequestParam(required = true) Role role,
+                                            @RequestParam(name = "email") String email) {
+        return ResponseEntity.ok().body(memberInfoService.getMemberAdmin(email, track, role));
+    }
+
 
 }

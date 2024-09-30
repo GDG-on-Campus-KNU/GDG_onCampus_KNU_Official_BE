ddc45e76a8c8f46a43f1a4f721a86c81b1a7cbe3
kwonssshyeon <kwonsh4396@gmail.com>
2024-09-30 23:06:54 +0900

    Fix: 테스트 워크플로, 환경변수 수정

--- src/test/java/com/gdsc_knu/official_homepage/OfficialHomepageApplicationTests.java
+++ src/test/java/com/gdsc_knu/official_homepage/OfficialHomepageApplicationTests.java
@@ -5,9 +5,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
-@TestPropertySource(properties = {
-        "spring.config.location = src/main/resources/application.yml, src/main/resources/application-test.yml"
-})
 class OfficialHomepageApplicationTests {
 
     @Test

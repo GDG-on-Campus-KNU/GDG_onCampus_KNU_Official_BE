c98d4973bb9f64596ed9ac63a51f9d31a6ff8700
kwonssshyeon <104684033+kwonssshyeon@users.noreply.github.com>
2024-10-07 17:13:27 +0900

    테크블로그 엔티티 작성 (#117)

---
+++ src/main/java/com/gdsc_knu/official_homepage/entity/post/Post.java
@@ -0,0 +1,46 @@
+package com.gdsc_knu.official_homepage.entity.post;
+
+import com.gdsc_knu.official_homepage.entity.Member;
+import com.gdsc_knu.official_homepage.entity.post.enumeration.Category;
+import com.gdsc_knu.official_homepage.entity.post.enumeration.PostStatus;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToMany(mappedBy = "post", cascade = CascadeType.ALL, orphanRemoval = true)
+    List<Comment> commentList = new ArrayList<>();
+
+    private String title;
+
+    @Column(length = 65535)
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Member member;
+
+    private String thumbnailUrl;
+
+    private Category category;
+
+    private PostStatus status;
+
+    private int likeCount;
+    private int commentCount;
+    private int sharedCount;
+
+    private LocalDateTime publishedAt;
+    private LocalDateTime modifiedAt;
+
+}
---
+++ src/main/java/com/gdsc_knu/official_homepage/entity/post/enumeration/PostStatus.java
@@ -0,0 +1,6 @@
+package com.gdsc_knu.official_homepage.entity.post.enumeration;
+
+public enum PostStatus {
+    // 임시 저장 , 저장
+    TEMPORAL, SAVED
+}

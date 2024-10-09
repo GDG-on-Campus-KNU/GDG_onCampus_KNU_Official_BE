62b11669376bf72586c0eee07010c84e1a10403a
kwonssshyeon <kwonsh4396@gmail.com>
2024-10-09 17:51:13 +0900

    Feat: 스케줄러를 통해 매일 자정 인기 게시글 초기화

--- src/main/java/com/gdsc_knu/official_homepage/OfficialHomepageApplication.java
+++ src/main/java/com/gdsc_knu/official_homepage/OfficialHomepageApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableJpaAuditing
 @EnableCaching
+@EnableScheduling
 public class OfficialHomepageApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/gdsc_knu/official_homepage/service/post/PostService.java
+++ src/main/java/com/gdsc_knu/official_homepage/service/post/PostService.java
@@ -5,13 +5,17 @@ import com.gdsc_knu.official_homepage.entity.post.Post;
 import com.gdsc_knu.official_homepage.entity.post.enumeration.Category;
 import com.gdsc_knu.official_homepage.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class PostService {
@@ -22,5 +26,15 @@ public class PostService {
     public List<PostResponse.Main> getTrendingPosts(Category category, int size) {
         List<Post> posts = postRepository.findTop5ByCategory(category, size);
         return posts.stream().map(PostResponse.Main::from).toList();
+    }
+
+    @Scheduled(cron = "0 0 0 * * ?")
+    public void invokeClearPost() {
+        clearTrendingPosts();
+    }
+
+    @CacheEvict(value = "trending-post", allEntries = true, beforeInvocation = true)
+    public void clearTrendingPosts() {
+        log.info("trending post 초기화");
     }
 }

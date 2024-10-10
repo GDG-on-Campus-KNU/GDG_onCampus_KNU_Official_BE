18d8c71dce37c4a3f70c4a5c40cdcf271a8aa7fa
chaejm55 <chaejm55@gmail.com>
2024-10-11 00:49:16 +0900

    Fix: dev 브랜치와 ResponseDTO 동기화

--- src/main/java/com/gdsc_knu/official_homepage/dto/post/PostResponse.java
+++ src/main/java/com/gdsc_knu/official_homepage/dto/post/PostResponse.java
@@ -6,6 +6,7 @@ import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import com.gdsc_knu.official_homepage.entity.Member;
+import com.gdsc_knu.official_homepage.entity.post.Post;
 import com.gdsc_knu.official_homepage.entity.post.enumeration.Category;
 import com.gdsc_knu.official_homepage.entity.post.enumeration.PostStatus;
 import lombok.AllArgsConstructor;
@@ -13,6 +14,7 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
@@ -22,35 +24,35 @@ import java.util.List;
 @NoArgsConstructor
 @Builder
 public class PostResponse {
-    private Long id;
+    @Getter
+    @Builder
+    @AllArgsConstructor
+    public static class Main implements Serializable {
+        private Long id;
+        private String title;
+        private String summary;
+        private String thumbnailUrl;
+        private String category;
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+        @JsonSerialize(using = LocalDateTimeSerializer.class)
+        @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+        private LocalDateTime createAt;
+        private int likeCount;
+        private int commentCount;
 
-    private List<CommentResponse> commentList = new ArrayList<>();
+        public static Main from(Post post) {
+            int length = Math.min(post.getContent().length(), 20);
+            return Main.builder()
+                    .id(post.getId())
+                    .title(post.getTitle())
+                    .summary(post.getContent().substring(0,length))
+                    .thumbnailUrl(post.getThumbnailUrl())
+                    .category(post.getCategory().name())
+                    .createAt(post.getPublishedAt())
+                    .likeCount(post.getLikeCount())
+                    .commentCount(post.getCommentCount())
+                    .build();
 
-    private String title;
-
-    private String content;
-
-    private Member member;
-
-    private String thumbnailUrl;
-
-    private Category category;
-
-    private PostStatus status;
-
-    private int likeCount;
-
-    private int commentCount;
-
-    private int sharedCount;
-
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
-    @JsonSerialize(using = LocalDateTimeSerializer.class)
-    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
-    private LocalDateTime publishedAt;
-
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
-    @JsonSerialize(using = LocalDateTimeSerializer.class)
-    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
-    private LocalDateTime modifiedAt;
+        }
+    }
 }

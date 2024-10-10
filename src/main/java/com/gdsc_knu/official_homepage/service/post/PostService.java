22665a4feca384672ad4b84a022281d3bc77e2f4
chaejm55 <chaejm55@gmail.com>
2024-10-11 01:49:13 +0900

    Feat: 게시글 CRUD 서비스 구현

--- src/main/java/com/gdsc_knu/official_homepage/service/post/PostService.java
+++ src/main/java/com/gdsc_knu/official_homepage/service/post/PostService.java
@@ -7,13 +7,13 @@ import com.gdsc_knu.official_homepage.entity.post.enumeration.Category;
 import java.util.List;
 
 public interface PostService {
-    void createPost(Long memberId, PostRequest.Create postRequestDto);
+    void createPost(Long memberId, PostRequest.Create postRequest);
 
     PostResponse.Main getPost(Long postId);
 
     List<PostResponse.Main> getPostList(Category category);
 
-    void updatePost(Long memberId, Long postId, PostRequest.Update postRequestDto);
+    void updatePost(Long memberId, Long postId, PostRequest.Update postRequest);
 
     void deletePost(Long memberId, Long postId);
 }
--- src/main/java/com/gdsc_knu/official_homepage/service/post/PostServiceImpl.java
+++ src/main/java/com/gdsc_knu/official_homepage/service/post/PostServiceImpl.java
@@ -2,11 +2,17 @@ package com.gdsc_knu.official_homepage.service.post;
 
 import com.gdsc_knu.official_homepage.dto.post.PostRequest;
 import com.gdsc_knu.official_homepage.dto.post.PostResponse;
+import com.gdsc_knu.official_homepage.entity.Member;
+import com.gdsc_knu.official_homepage.entity.enumeration.Role;
+import com.gdsc_knu.official_homepage.entity.post.Post;
 import com.gdsc_knu.official_homepage.entity.post.enumeration.Category;
+import com.gdsc_knu.official_homepage.exception.CustomException;
+import com.gdsc_knu.official_homepage.exception.ErrorCode;
 import com.gdsc_knu.official_homepage.repository.MemberRepository;
 import com.gdsc_knu.official_homepage.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -17,27 +23,63 @@ public class PostServiceImpl implements PostService {
     private final MemberRepository memberRepository;
 
     @Override
-    public void createPost(Long memberId, PostRequest.Create postRequestDto) {
-
+    @Transactional
+    public void createPost(Long memberId, PostRequest.Create postRequest) {
+        Member member = memberRepository.findById(memberId)
+                .orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_FOUND));
+        Post post = new Post(postRequest, member);
+        postRepository.save(post);
     }
 
     @Override
+    @Transactional(readOnly = true)
     public PostResponse.Main getPost(Long postId) {
-        return null;
+        Post post = postRepository.findById(postId)
+                .orElseThrow(() -> new CustomException(ErrorCode.POST_NOT_FOUND));
+        if (!post.isSaved()) {
+            throw new CustomException(ErrorCode.POST_NOT_FOUND);
+        }
+        return PostResponse.Main.from(post);
     }
 
     @Override
+    @Transactional(readOnly = true)
     public List<PostResponse.Main> getPostList(Category category) {
-        return null;
+        List<Post> postList;
+        if (category != null) {
+            postList = postRepository.findAllByCategory(category);
+        }
+        else {
+            postList = postRepository.findAll();
+        }
+        return postList.stream()
+                .filter(Post::isSaved)
+                .map(PostResponse.Main::from)
+                .toList();
     }
 
     @Override
-    public void updatePost(Long memberId, Long postId, PostRequest.Update postRequestDto) {
-
+    @Transactional
+    public void updatePost(Long memberId, Long postId, PostRequest.Update postRequest) {
+        Post post = postRepository.findById(postId)
+                .orElseThrow(() -> new CustomException(ErrorCode.POST_NOT_FOUND));
+        if (!post.getMember().getId().equals(memberId)) {
+            throw new CustomException(ErrorCode.POST_FORBIDDEN);
+        }
+        post.update(postRequest);
     }
 
     @Override
+    @Transactional
     public void deletePost(Long memberId, Long postId) {
-
+        Post post = postRepository.findById(postId)
+                .orElseThrow(() -> new CustomException(ErrorCode.POST_NOT_FOUND));
+        Member member = memberRepository.findById(memberId)
+                .orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_FOUND));
+        if (!post.getMember().getId().equals(memberId) && !member.getRole().equals(Role.ROLE_CORE)) {
+            // 코어는 게시글 삭제도 가능
+            throw new CustomException(ErrorCode.POST_FORBIDDEN);
+        }
+        postRepository.delete(post);
     }
 }

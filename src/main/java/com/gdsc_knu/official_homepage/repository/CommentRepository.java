89668364c27a65ce0dc272c7f582261e079423e6
kwonssshyeon <kwonsh4396@gmail.com>
2024-10-20 17:08:50 +0900

    Test: 게시글 댓글 그룹 생성, 조회 쿼리 테스트

--- src/main/java/com/gdsc_knu/official_homepage/repository/CommentRepository.java
+++ src/main/java/com/gdsc_knu/official_homepage/repository/CommentRepository.java
@@ -5,11 +5,12 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
     @Query("SELECT c " +
             "FROM Comment c " +
             "WHERE c.post.id = :postId "+
             "ORDER BY c.parent.id ASC, c.createAt ASC")
-    Page<Comment> findCommentAndReply(Pageable pageable, Long postId);
+    Page<Comment> findCommentAndReply(Pageable pageable, @Param(value = "postId") Long postId);
 }

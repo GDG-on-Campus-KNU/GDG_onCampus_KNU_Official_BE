3cc7d8c9de8d769b47c40a6891c74dbf7d6d1d93
kwonssshyeon <kwonsh4396@gmail.com>
2024-11-06 23:26:07 +0900

    Test: 멤버 -> 팀 조회 테스트 작성

--- src/test/java/com/gdsc_knu/official_homepage/member/MemberServiceTest.java
+++ src/test/java/com/gdsc_knu/official_homepage/member/MemberServiceTest.java
@@ -1,19 +1,23 @@
 package com.gdsc_knu.official_homepage.member;
 
 import com.gdsc_knu.official_homepage.dto.member.MemberRequest;
+import com.gdsc_knu.official_homepage.dto.team.TeamResponse;
 import com.gdsc_knu.official_homepage.entity.Member;
-import com.gdsc_knu.official_homepage.entity.MemberTeam;
 import com.gdsc_knu.official_homepage.entity.Team;
 import com.gdsc_knu.official_homepage.entity.enumeration.Role;
 import com.gdsc_knu.official_homepage.entity.enumeration.Track;
 import com.gdsc_knu.official_homepage.repository.member.MemberRepository;
+import com.gdsc_knu.official_homepage.repository.team.TeamRepository;
 import com.gdsc_knu.official_homepage.service.MemberService;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.dao.DataIntegrityViolationException;
 
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -23,6 +27,12 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 public class MemberServiceTest {
     @Autowired private MemberService memberService;
     @Autowired private MemberRepository memberRepository;
+    @Autowired private TeamRepository teamRepository;
+
+    @AfterEach
+    void tearDown() {
+        memberRepository.deleteAll();
+    }
 
     @Test
     @DisplayName("중복된 전화번호로 회원가입하는 경우 반영되지 않고, GUEST 권한을 가질 수 없다.")
@@ -46,20 +56,47 @@ public class MemberServiceTest {
 
 
     @Test
-    @DisplayName("사용자의 팀 조회시 실제 배정되지 않은 팀은 조회되지 않는다.")
+    @DisplayName("사용자의 팀 조회시 실제 배정되지 않은 팀(편의상 부모팀)은 조회되지 않는다.")
     void getMemberTeam() {
-
+        // given
+        Member member = createMember(1L);
+        memberRepository.save(member);
+        String parentName = "미배정부모팀", assignedName = "배정 1팀";
+        Team parent = parentTeam(1L, parentName);
+        Team assignedTeam = assignedTeam(2L, parent, assignedName);
+        for (Team team : List.of(parent, assignedTeam)){
+            team.addMember(member);
+            teamRepository.save(team);
+        }
+        // when
+        List<TeamResponse.Main> response =  memberService.getMemberTeamInfo(member.getId());
+        // then
+        assertThat(response.size()).isEqualTo(1L);
+        assertThat(response.get(0).getTeamName()).isEqualTo(assignedName);
     }
 
-    // TODO: 테스트 중 team 생성에 member - team 간 관계매핑이 원자적으로 이루어지지 않는것을 발견, 관련 로직 수정 필요
     @Test
     @DisplayName("사용자의 팀 조회시 최근순으로 조회된다.")
     void getMemberTeamOrder() {
-        Team notAssignedParent = parentTeam();
-        Team assignedParent = parentTeam();
-        Team realAssigned = assignedTeam(assignedParent);
+        // given
+        Member member = createMember(1L);
+        memberRepository.save(member);
 
-
+        String teamName = "테스트";
+        Team parent1 = parentTeam(1L, teamName);
+        Team parent2 = parentTeam(2L, teamName);
+        Team child1 = assignedTeam(3L, parent1,teamName);
+        Team child2 = assignedTeam(4L, parent1,teamName);
+        Team child3 = assignedTeam(5L, parent2,teamName);
+        for (Team team : List.of(parent1, parent2, child1, child2, child3)){
+            team.addMember(member);
+            teamRepository.save(team);
+        }
+        // when
+        List<TeamResponse.Main> response =  memberService.getMemberTeamInfo(member.getId());
+        // then
+        assertThat(response.size()).isEqualTo(3);
+        assertThat(response.stream().map(TeamResponse.Main::getId)).containsExactly(5L, 4L, 3L);
     }
 
     private Member createMember(Long id) {
@@ -71,23 +108,19 @@ public class MemberServiceTest {
                 .build();
     }
 
-    private Team parentTeam() {
+    private Team parentTeam(Long id, String teamName) {
         return Team.builder()
+                .id(id)
+                .teamName(teamName)
                 .parent(null)
                 .build();
     }
 
-    private Team assignedTeam(Team parent) {
-        Team team = Team.builder()
-                .build();
-        return team;
-    }
-
-    private MemberTeam createMemberTeam(Member member, Team team) {
-        return  MemberTeam.builder()
-                .member(member)
-                .team(team)
+    private Team assignedTeam(Long id, Team parent, String teamName) {
+        return Team.builder()
+                .id(id)
+                .parent(parent)
+                .teamName(teamName)
                 .build();
     }
-
 }

3e34a6f815054c9edb7d5362a0ad6133cc994b1e
kwonssshyeon <kwonsh4396@gmail.com>
2024-11-06 22:45:25 +0900

    Test: 팀생성시 팀-멤버, 하위팀-상위팀 연관관계 테스트 작성

---
+++ src/test/java/com/gdsc_knu/official_homepage/team/AdminTeamServiceTest.java
@@ -0,0 +1,86 @@
+package com.gdsc_knu.official_homepage.team;
+
+import com.gdsc_knu.official_homepage.dto.admin.team.AdminTeamRequest;
+import com.gdsc_knu.official_homepage.entity.Member;
+import com.gdsc_knu.official_homepage.entity.Team;
+import com.gdsc_knu.official_homepage.entity.enumeration.Role;
+import com.gdsc_knu.official_homepage.entity.enumeration.Track;
+import com.gdsc_knu.official_homepage.repository.member.MemberRepository;
+import com.gdsc_knu.official_homepage.repository.team.MemberTeamRepository;
+import com.gdsc_knu.official_homepage.repository.team.TeamRepository;
+import com.gdsc_knu.official_homepage.service.admin.AdminTeamServiceImpl;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class AdminTeamServiceTest {
+    @InjectMocks private AdminTeamServiceImpl adminTeamService;
+    @Mock private MemberRepository memberRepository;
+    @Mock private TeamRepository teamRepository;
+    @Mock private MemberTeamRepository memberTeamRepository;
+
+    @Test
+    @DisplayName("상위팀 생성 시 track이 같은 모든 멤버는 해당 팀에 포함된다.")
+    void createParentTeamAndConnectMembers() {
+        // given
+        Member member1 = createMember(1L);
+        Member member2 = createMember(2L);
+        when(memberRepository.findAll()).thenReturn(new ArrayList<>(List.of(member1, member2)));
+        String teamName = "팀 이름";
+        // when
+        adminTeamService.createParentTeam(new AdminTeamRequest.Create(teamName, null));
+        // then
+        assertThat(member1.getMemberTeams().size()).isEqualTo(1L);
+        assertThat(member1.getMemberTeams().get(0).getTeam().getTeamName()).isEqualTo(teamName);
+        assertThat(member1.getMemberTeams().get(0).getMember()).isEqualTo(member1);
+        assertThat(member2.getMemberTeams().size()).isEqualTo(1L);
+        assertThat(member2.getMemberTeams().get(0).getTeam().getTeamName()).isEqualTo(teamName);
+        assertThat(member2.getMemberTeams().get(0).getMember()).isEqualTo(member2);
+    }
+
+    @Test
+    @DisplayName("하위팀 생성시 상위팀과 연관관계가 정상적으로 매핑된다.")
+    void createSubTeamAndCommentParent() {
+        // given
+        Team parent = createTeam(1L, null);
+        when(teamRepository.findById(parent.getId())).thenReturn(Optional.of(parent));
+
+        // when
+        adminTeamService.createSubTeam(parent.getId());
+
+        // then
+        assertThat(parent.getSubTeams().size()).isEqualTo(1L);
+        assertThat(parent.getSubTeams().get(0).getParent()).isEqualTo(parent);
+        assertThat(parent.getSubTeams().get(0).getTeamName()).isEqualTo(String.format("%s 1팀", parent.getTeamName()));
+
+    }
+
+    private Member createMember(Long id) {
+        return  Member.builder()
+                .id(id)
+                .email("test@email.com")
+                .name("테스트 유저")
+                .track(Track.BACK_END)
+                .role(Role.ROLE_MEMBER)
+                .build();
+    }
+
+    private Team createTeam(Long id, Team parent) {
+        return Team.builder()
+                .id(id)
+                .teamName("팀 이름")
+                .parent(parent)
+                .build();
+    }
+}

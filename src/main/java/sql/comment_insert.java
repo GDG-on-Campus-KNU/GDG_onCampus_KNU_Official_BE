865002216bce95238ab494c84367acaf879b9528
kwonssshyeon <kwonsh4396@gmail.com>
2024-12-08 16:56:51 +0900

    Feat: 더미데이터 삽입 편의 기능 - post

---
+++ src/main/java/sql/comment_insert.java
@@ -0,0 +1,60 @@
+package sql;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+@Slf4j
+public class comment_insert {
+    private static final int MAX_POST = 1000;
+    private static final int MAX_MEMBER = 1000;
+    private static final int MAX_GROUP = 10;
+    private static final int CHILD_PER_GROUP = 10;
+
+    public static void main(String[] args) throws SQLException {
+        DataSource dataSource = new DataSource();
+        Connection conn = dataSource.open();
+        String psql = "INSERT IGNORE INTO comment(id, post_id, author_id, parent_id, content, author_name) " +
+                      "VALUES (?,?,?,?,?,?)";
+        PreparedStatement stmt = conn.prepareStatement(psql);
+
+        try {
+            insertPost(stmt);
+            conn.setAutoCommit(false);
+            stmt.executeBatch();
+            conn.commit();
+            log.info("success for insert comment");
+        }
+        catch (SQLException e) {
+            conn.rollback();
+            log.error(e.getMessage());
+        }
+        finally {
+            conn.setAutoCommit(true);
+            dataSource.free();
+            stmt.close();
+        }
+    }
+
+
+    private static void insertPost(PreparedStatement stmt) throws SQLException {
+        int id = 1;
+        int group = 1;
+        for (int postId=1; postId<=MAX_POST; postId++) {
+            for (int groupId=1; groupId<=MAX_GROUP; groupId++) {
+                for (int childId=1; childId<=CHILD_PER_GROUP; childId++) {
+                    stmt.setLong(1, id++);
+                    stmt.setLong(2, postId);
+                    stmt.setLong(3, id%MAX_MEMBER+1);
+                    stmt.setLong(4, group);
+                    stmt.setString(5, "댓글내용");
+                    stmt.setString(6, "작성자이름");
+                    stmt.addBatch();
+                }
+                group+=CHILD_PER_GROUP;
+            }
+        }
+    }
+}
---
+++ src/main/java/sql/post_insert.java
@@ -0,0 +1,66 @@
+package sql;
+
+import com.gdsc_knu.official_homepage.entity.post.enumeration.Category;
+import com.gdsc_knu.official_homepage.entity.post.enumeration.PostStatus;
+import lombok.extern.slf4j.Slf4j;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+@Slf4j
+public class post_insert {
+    private static final int MAX = 1000;
+    private static final int MAX_MEMBER = 100;
+
+
+    public static void main(String[] args) throws SQLException {
+        DataSource dataSource = new DataSource();
+        Connection conn = dataSource.open();
+        String psql = "INSERT IGNORE INTO post(id, title, content, member_id, category, status, like_count, comment_count, shared_count) " +
+                "VALUES (?,?,?,?,?,?,?,?,?)";
+        PreparedStatement stmt = conn.prepareStatement(psql);
+
+        try {
+            insertPost(stmt);
+            conn.setAutoCommit(false);
+            stmt.executeBatch();
+            conn.commit();
+            log.info("success for insert post");
+        }
+        catch (SQLException e) {
+            conn.rollback();
+            log.error(e.getMessage());
+        }
+        finally {
+            conn.setAutoCommit(true);
+            dataSource.free();
+            stmt.close();
+        }
+    }
+
+
+    private static void insertPost(PreparedStatement stmt) throws SQLException {
+        for (int i=1; i<=MAX; i++) {
+            stmt.setLong(1, i);
+            stmt.setString(2, "제목");
+            stmt.setString(3, "본문내용");
+            stmt.setLong(4, i%MAX_MEMBER+1);
+            stmt.setString(5, getCategory(i));
+            stmt.setString(6, getStatus(i));
+            stmt.setInt(7,0);
+            stmt.setInt(8,0);
+            stmt.setInt(9,0);
+            stmt.addBatch();
+        }
+    }
+
+    private static String getStatus(int i) {
+        return i < 100 ? PostStatus.TEMPORAL.name() : PostStatus.SAVED.name();
+    }
+
+    private static final int categorySize = Category.values().length;
+    private static String getCategory(int i) {
+        return Category.values()[i%categorySize].name();
+    }
+}

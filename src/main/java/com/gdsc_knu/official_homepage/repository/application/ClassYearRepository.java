2bdca34725c1b756e0f96da659307eb9947f0232
chaejm55 <chaejm55@gmail.com>
2024-12-18 19:36:17 +0900

    Feat: 기수 레포지토리 작성

---
+++ src/main/java/com/gdsc_knu/official_homepage/repository/application/ClassYearRepository.java
@@ -0,0 +1,7 @@
+package com.gdsc_knu.official_homepage.repository.application;
+
+import com.gdsc_knu.official_homepage.entity.ClassYear;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClassYearRepository extends JpaRepository<ClassYear, Long> {
+}

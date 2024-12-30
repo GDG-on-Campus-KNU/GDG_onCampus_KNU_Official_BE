20bdd8fa44ec1149fe77f6fdbefeb418a142024c
kwonssshyeon <kwonsh4396@gmail.com>
2024-12-30 21:54:27 +0900

    Feat: 통계 쿼리에 기수 필터링 추가 (-> querydsl 변경)

--- src/main/java/com/gdsc_knu/official_homepage/repository/application/ApplicationQueryFactory.java
+++ src/main/java/com/gdsc_knu/official_homepage/repository/application/ApplicationQueryFactory.java
@@ -1,10 +1,18 @@
 package com.gdsc_knu.official_homepage.repository.application;
 
+import com.gdsc_knu.official_homepage.dto.admin.application.ApplicationStatisticType;
+import com.gdsc_knu.official_homepage.dto.admin.application.ApplicationTrackType;
 import com.gdsc_knu.official_homepage.entity.application.Application;
 import com.gdsc_knu.official_homepage.entity.enumeration.Track;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
+import java.util.List;
+
 public interface ApplicationQueryFactory {
     Page<Application> findAllApplicationsByOption(Pageable pageable, Track track, Boolean isMarked, Long classYearId);
+
+    ApplicationStatisticType getStatistics(Long classYearId);
+
+    List<ApplicationTrackType> getGroupByTrack(Long classYearId);
 }
--- src/main/java/com/gdsc_knu/official_homepage/repository/application/ApplicationQueryFactoryImpl.java
+++ src/main/java/com/gdsc_knu/official_homepage/repository/application/ApplicationQueryFactoryImpl.java
@@ -1,10 +1,13 @@
 package com.gdsc_knu.official_homepage.repository.application;
 
+import com.gdsc_knu.official_homepage.dto.admin.application.ApplicationStatisticType;
+import com.gdsc_knu.official_homepage.dto.admin.application.ApplicationTrackType;
 import com.gdsc_knu.official_homepage.entity.application.Application;
 import com.gdsc_knu.official_homepage.entity.application.QApplication;
-import com.gdsc_knu.official_homepage.entity.enumeration.ApplicationStatus;
 import com.gdsc_knu.official_homepage.entity.enumeration.Track;
+import com.querydsl.core.types.Projections;
 import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.core.types.dsl.CaseBuilder;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -13,6 +16,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import static com.gdsc_knu.official_homepage.entity.enumeration.ApplicationStatus.*;
 
 
 @Repository
@@ -23,7 +27,7 @@ public class ApplicationQueryFactoryImpl implements ApplicationQueryFactory{
     public Page<Application> findAllApplicationsByOption(Pageable pageable, Track track, Boolean isMarked, Long classYearId) {
         List<Application> applications = jpaQueryFactory
                 .selectFrom(QApplication.application)
-                .where(QApplication.application.applicationStatus.ne(ApplicationStatus.TEMPORAL)
+                .where(QApplication.application.applicationStatus.ne(TEMPORAL)
                         .and(eqTrack(track))
                         .and(eqIsMarked(isMarked))
                         .and(eqClassYear(classYearId)))
@@ -35,7 +39,7 @@ public class ApplicationQueryFactoryImpl implements ApplicationQueryFactory{
         Long total = jpaQueryFactory
                 .select(QApplication.application.count())
                 .from(QApplication.application)
-                .where(QApplication.application.applicationStatus.ne(ApplicationStatus.TEMPORAL)
+                .where(QApplication.application.applicationStatus.ne(TEMPORAL)
                         .and(eqTrack(track))
                         .and(eqIsMarked(isMarked))
                         .and(eqClassYear(classYearId)))
@@ -43,6 +47,38 @@ public class ApplicationQueryFactoryImpl implements ApplicationQueryFactory{
         return new PageImpl<>(applications, pageable, total == null ? 0 : total);
     }
 
+    @Override
+    public ApplicationStatisticType getStatistics(Long classYearId) {
+        return jpaQueryFactory
+            .select(Projections.fields(ApplicationStatisticType.class,
+                new CaseBuilder().when(QApplication.application.applicationStatus.ne(TEMPORAL))
+                        .then(1).otherwise(0).sum().as("total"),
+                new CaseBuilder().when(QApplication.application.applicationStatus.ne(TEMPORAL)
+                        .and(QApplication.application.isOpened.eq(true)))
+                        .then(1).otherwise(0).sum().as("openCount"),
+                new CaseBuilder().when(QApplication.application.applicationStatus.eq(APPROVED))
+                        .then(1).otherwise(0).sum().as("approvedCount"),
+                new CaseBuilder().when(QApplication.application.applicationStatus.eq(REJECTED))
+                        .then(1).otherwise(0).sum().as("rejectedCount")
+            ))
+            .from(QApplication.application)
+            .where(eqClassYear(classYearId))
+            .fetchOne();
+    }
+
+    @Override
+    public List<ApplicationTrackType> getGroupByTrack(Long classYearId) {
+        return jpaQueryFactory
+                .select(Projections.fields(ApplicationTrackType.class,
+                        QApplication.application.track.as("track"),
+                        QApplication.application.count().as("count")
+                ))
+                .from(QApplication.application)
+                .where(QApplication.application.applicationStatus.ne(TEMPORAL)
+                        .and(eqClassYear(classYearId)))
+                .groupBy(QApplication.application.track)
+                .fetch();
+    }
 
 
     private BooleanExpression eqTrack(Track track) {
